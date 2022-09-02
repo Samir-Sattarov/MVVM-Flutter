4ef2f740fd795e5d5a1104ee8228549384318949
Samir-Sattarov <samirsattarov2004@gmail.com>
2022-09-02 11:56:35 +0500

    initial

---
+++ android/app/src/main/java/com/flutter/mvvm/flutter_mvvm_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.flutter.mvvm.flutter_mvvm_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/internal/application.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../view/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
