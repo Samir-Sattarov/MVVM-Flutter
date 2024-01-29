443cd14d088aa485456ca25fdb671653bbda89a7
Samir-Sattarov <samirsattarov2004@gmail.com>
2024-01-29 19:32:01 +0500

    V2

--- lib/view/screens/post_view.dart
+++ lib/view/screens/post_view.dart
@@ -1,8 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_example/core/widgets/error_flash_bar.dart';
+import 'package:flutter_mvvm_example/core/widgets/success_flash_bar.dart';
 import 'package:provider/provider.dart';
 
 import '../../view_model/post_view_model.dart';
 import '../widget/post_card_widget.dart';
+
+
+//
+// class PostViewModel extends ChangeNotifier {
+//   final PostRepository postRepository;
+//
+//   PostViewModel(this.postRepository);
+//
+//   bool enableError = false;
+//   bool enableSuccess = false;
+//
+//   late PostResults _results = PostResults.empty();
+//
+//   PostResults get results => _results;
+//
+//   load() async {
+//     try {
+//       final response = await postRepository.getPostsResults();
+//       _viewSuccess();
+//
+//       _results = response;
+//
+//       notifyListeners();
+//     } catch (error) {
+//       _viewError();
+//     }
+//   }
+//
+//   _viewError() async {
+//     enableError = true;
+//     notifyListeners();
+//     await Future.delayed(
+//       const Duration(seconds: 1),
+//           () {
+//         enableError = false;
+//         notifyListeners();
+//       },
+//     );
+//   }
+//
+//   _viewSuccess() async {
+//     enableSuccess = true;
+//     notifyListeners();
+//     await Future.delayed(
+//       const Duration(seconds: 1),
+//           () {
+//         enableSuccess = false;
+//         notifyListeners();
+//       },
+//     );
+//   }
+// }
+
+
 
 class PostView extends StatefulWidget {
   const PostView({Key? key}) : super(key: key);
@@ -21,9 +77,24 @@ class _PostViewState extends State<PostView> {
   @override
   Widget build(BuildContext context) {
     final postProvider = context.read<PostViewModel>();
+    final postProviderWatch = context.watch<PostViewModel>();
+
+    if (mounted) {
+      Future.delayed(
+        Duration.zero,
+        () {
+          if (postProviderWatch.enableSuccess) {
+            SuccessFlushBar("Success").show(context);
+          }
+          if (postProviderWatch.enableError) {
+            ErrorFlushBar("Error").show(context);
+          }
+        },
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-
         title: const Text("Posts View"),
       ),
       body: SizedBox(
--- lib/view_model/post_view_model.dart
+++ lib/view_model/post_view_model.dart
@@ -5,20 +5,53 @@ import 'package:flutter_mvvm_example/core/repositories/post/post_repository.dart
 
 import '../model/post_results.dart';
 
+
 class PostViewModel extends ChangeNotifier {
   final PostRepository postRepository;
 
   PostViewModel(this.postRepository);
+
+  bool enableError = false;
+  bool enableSuccess = false;
 
   late PostResults _results = PostResults.empty();
 
   PostResults get results => _results;
 
   load() async {
-    final response = await postRepository.getPostsResults();
+    try {
+      final response = await postRepository.getPostsResults();
+      _viewSuccess();
 
-    _results = response;
+      _results = response;
 
+      notifyListeners();
+    } catch (error) {
+      _viewError();
+    }
+  }
+
+  _viewError() async {
+    enableError = true;
     notifyListeners();
+    await Future.delayed(
+      const Duration(seconds: 1),
+          () {
+        enableError = false;
+        notifyListeners();
+      },
+    );
+  }
+
+  _viewSuccess() async {
+    enableSuccess = true;
+    notifyListeners();
+    await Future.delayed(
+      const Duration(seconds: 1),
+          () {
+        enableSuccess = false;
+        notifyListeners();
+      },
+    );
   }
 }
